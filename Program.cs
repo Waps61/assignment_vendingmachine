ebec7ef387d6954ef8d0df76f88cfccd2e55ddc4
Stefan Moring <stefan.moring@gain.nl>
2021-08-19 09:11:28 +0200

    refactored vendingmachine to support injection

---
+++ Program.cs
@@ -0,0 +1,15 @@
+namespace assigment_vendingmachine
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var productSelector = new ProductSelector();
+            var stockManager = new StockManager();
+
+            var vendingMachine = new VendingMachine(productSelector, stockManager);
+
+            vendingMachine.Start();
+        }
+    }
+}
--- VendingMachine.cs
+++ VendingMachine.cs
@@ -1,24 +1,26 @@
-using System;
+﻿using System;
 
 namespace assigment_vendingmachine
 {
-
-  class VendingMachine
-  {
-    static string version = "0.1";
-
-    static ProductSelector myProductSelector = new ProductSelector();
-    static StockManager myStockMananger = new StockManager();
-
-    private CoinModule cashModule;
-
-    static void Main(string[] args)
+    public class VendingMachine
     {
-      Console.WriteLine("Welcome to VendingMachine V "+ version);
-      Console.WriteLine(myProductSelector.showKeyboard());
-      Console.WriteLine(myStockMananger.showStock());
-      Console.WriteLine("Select a product...");
-      Console.WriteLine("Selected : "+myProductSelector.readSelection());
+        private static readonly string version = "0.1";
+
+       
+        private readonly ProductSelector productSelector;
+        private readonly StockManager stockManager;
+        public VendingMachine(ProductSelector productSelector, StockManager stockManager)
+        {
+            this.productSelector = productSelector;
+            this.stockManager = stockManager;
+        }
+        public void Start()
+        {
+            Console.WriteLine("Welcome to VendingMachine V " + version);
+            Console.WriteLine(productSelector.showKeyboard());
+            Console.WriteLine(stockManager.showStock());
+            Console.WriteLine("Select a product...");
+            Console.WriteLine("Selected : " + productSelector.readSelection());
+        }
     }
-  }
 }
