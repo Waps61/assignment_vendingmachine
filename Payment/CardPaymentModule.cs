928f72c40b5ddaea9e3ac7e95a2ce5c1b968c52f
Stefan Moring <stefan.moring@gain.nl>
2021-08-19 12:03:17 +0200

    Added cardpayment module

---
+++ Payment/CardPaymentModule.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Threading;
+
+namespace assignment_vendingmachine.Payment
+{
+    public class CardPaymentModule : IPaymentModule
+    {
+        public bool StartTransaction(int amount)
+        {
+            Console.Write("Please tap your card (type tap): ");
+            var line = Console.ReadLine();
+            if (line != "tap")
+                return false;
+
+            ProcessPayment();
+            if(PaymentSuccessful())
+            {
+                return true;
+            }
+            else
+            {
+                Console.WriteLine("Insufficient funds!");
+                return false;
+            }
+        }
+
+        private bool PaymentSuccessful()
+        {
+            var rnd = new Random();
+            return rnd.NextDouble() < 0.8;
+        }
+
+        private static void ProcessPayment()
+        {
+            Console.Write("Processing");
+            for (int k = 0; k < 3; k++)
+            {
+                Thread.Sleep(500);
+                Console.Write(".");
+            }
+            Console.WriteLine(" Done!");
+        }
+    }
+}
