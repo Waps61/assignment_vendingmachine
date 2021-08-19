a72a0ef1d27b38596d4687d939c8560c314eb62f
Stefan Moring <stefan.moring@gain.nl>
2021-08-19 09:19:43 +0200

    extracted public StockManager methods to interface IStockManager

--- VendingMachine.cs
+++ VendingMachine.cs
@@ -7,14 +7,19 @@ namespace assigment_vendingmachine
         string ShowKeyboard();
         string ReadSelection();
     }
+    public interface IStockManager
+    {
+        string ShowStock();
+    }
+
     public class VendingMachine
     {
         private static readonly string version = "0.1";
 
-       
         private readonly IProductSelector productSelector;
-        private readonly StockManager stockManager;
-        public VendingMachine(IProductSelector productSelector, StockManager stockManager)
+        private readonly IStockManager stockManager;
+
+        public VendingMachine(IProductSelector productSelector, IStockManager stockManager)
         {
             this.productSelector = productSelector;
             this.stockManager = stockManager;
@@ -23,7 +28,7 @@ namespace assigment_vendingmachine
         {
             Console.WriteLine("Welcome to VendingMachine V " + version);
             Console.WriteLine(productSelector.ShowKeyboard());
-            Console.WriteLine(stockManager.showStock());
+            Console.WriteLine(stockManager.ShowStock());
             Console.WriteLine("Select a product...");
             Console.WriteLine("Selected : " + productSelector.ReadSelection());
         }
